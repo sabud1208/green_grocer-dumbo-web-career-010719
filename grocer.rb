8b06c80082e0697d31d3b9899772d7951de97c8e
Stephanie Abud <sabud1208@gmail.com>
2019-01-06 18:42:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,10 +4,22 @@ require "pry"
 #  {"PEANUTBUTTER"=>{:price=>3.0, :clearance=>true}},
 #  {"ALMONDS"=>{:price=>9.0, :clearance=>false}}]
 
+# result = consolidate_cart(cart)
+# expect(result["AVOCADO"][:price]).to eq(3.00)
+# expect(result["AVOCADO"][:clearance]).to eq(true)
+# expect(result["AVOCADO"][:count]).to eq(2)
+# 
+# expect(result["KALE"][:price]).to eq(3.00)
+# expect(result["KALE"][:clearance]).to eq(false)
+# expect(result["KALE"][:count]).to eq(1)
+# end
+
 def consolidate_cart(cart)
+  new_hash = {}
    cart.each do |item, attributes|
      if attributes[:count] == nil
         attributes[:count] = 1
+        new_hash[:item][:count]
       else
         attributes[:count] += 1
    end
