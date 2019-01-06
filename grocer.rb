1492c442072cea9973caad86d9509b866a583172
Stephanie Abud <sabud1208@gmail.com>
2019-01-06 19:45:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,6 @@ def apply_coupons(cart, coupons)
         if coupon[:item] == item
           discount["#{item} W/COUPON"] = {:price => coupon[:cost], :count => attributes[:count] / coupon[:num] }
           attributes[:count] %= coupon[:num]
-          binding.pry
           # we want to divide the count in the cart by the num in coupons
         # end
       end
