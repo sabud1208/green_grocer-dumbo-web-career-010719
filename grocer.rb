314940b08a665c6db193215310121adc1b6b39f6
Stephanie Abud <sabud1208@gmail.com>
2019-01-06 19:01:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ def apply_coupons(cart, coupons)
     item.each do |name, attributes|
       coupons.each do |coupon|
         if coupon[:item] == name
-          
+          puts "it worked"
         end
       end
     end
